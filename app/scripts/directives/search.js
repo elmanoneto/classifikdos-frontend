f2e75ecb2175e10affbae727b5121d642a40c4f0
elmano <elmanoneto@gmail.com>
2013-11-29 02:56:47 -0300

    search input

--- app/scripts/directives/search.js
+++ app/scripts/directives/search.js
@@ -6,19 +6,20 @@ angular.module('classifikdosApp')
       restrict: 'A',
       link: function search(scope, element, attrs) {
         var change = 0;
-          
+        $(this).stop();
         $('.input-search').focus(function () {      
           if(change != 1){
-            $(this).animate({width:'+=200px',left:'-=20px'}, 'linear');
+            $(this).animate({width:'+=285px'}, 'linear');
           }
           
           change = 1;
         });
 
         $('.input-search').focusout(function () {
-          
+
+          $(this).stop();
           if(change != 0){
-            $(this).animate({width:'-=200px',left:'-=10px'}, 'linear');
+            $(this).animate({width:'-=285px'}, 'linear');
           }
           
           change = 0;
